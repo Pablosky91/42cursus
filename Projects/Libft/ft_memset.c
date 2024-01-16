8a4b2922dbee0a0a5bc41c5b9ee3f493e0bdfe0e
Pablo <pablodelolmodecasas@gmail.com>
2024-01-16 18:04:54 +0100

    memset

--- Projects/Libft/ft_memset.c
+++ Projects/Libft/ft_memset.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 19:45:00 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/01/16 17:17:30 by pdel-olm         ###   ########.fr       */
+/*   Created: 2024/01/16 17:05:30 by pdel-olm          #+#    #+#             */
+/*   Updated: 2024/01/16 18:02:00 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdio.h>
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			aux;
+	unsigned char	*copy;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-void	*ft_memset(void *s, int c, size_t n);
-size_t	ft_strlen(const char *s);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-
-#endif
+	copy = (unsigned char *) s;
+	aux = 0;
+	while (aux < n)
+	{
+		copy[aux] = c;
+		aux++;
+	}
+	return (s);
+}
