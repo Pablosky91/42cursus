70a81fc44704a65a3fb179feeedfc8048727757f
Pablo <pablodelolmodecasas@gmail.com>
2024-01-25 22:28:26 +0100

    memchr and memcmp

--- Projects/Libft/ft_memchr.c
+++ Projects/Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 20:56:28 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/01/25 21:00:57 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/01/25 22:23:05 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	size_t			i;
+	unsigned char	*copy;
+
+	i = 0;
+	copy = (unsigned char *) s;
+	while (i < n)
+	{
+		if (copy[i] == (unsigned char)c)
+			return ((unsigned char *)&copy[i]);
+		i++;
+	}
+	return (0);
 }
--- Projects/Libft/ft_memcmp.c
+++ Projects/Libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 20:56:30 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/01/25 21:00:49 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/01/25 22:17:33 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	size_t			i;
+	unsigned char	*s1_copy;
+	unsigned char	*s2_copy;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	s1_copy = (unsigned char *) s1;
+	s2_copy = (unsigned char *) s2;
+	while (s1_copy[i] == s2_copy[i] && i + 1 < n)
+		i++;
+	return (s1_copy[i] - s2_copy[i]);
 }
