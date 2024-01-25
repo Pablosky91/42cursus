06a50b35cdce7fb59b683d9d7ee481a56f78f7a4
Pablo <pablodelolmodecasas@gmail.com>
2024-01-25 22:28:44 +0100

    refactor

--- Projects/Libft/ft_memcpy.c
+++ Projects/Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 19:41:31 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/01/24 01:34:21 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/01/25 22:25:33 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	char	*dst_copy;
-	char	*src_copy;
+	unsigned char	*dst_copy;
+	unsigned char	*src_copy;
 
 	if (dst == 0 && src == 0)
 		return (dst);
 	i = 0;
-	dst_copy = (char *) dst;
-	src_copy = (char *) src;
+	dst_copy = (unsigned char *) dst;
+	src_copy = (unsigned char *) src;
 	while (i < n)
 	{
 		ft_memset(&dst_copy[i], src_copy[i], 1);
