889eb7bbf9dcbd9dd4e48f679317d1a29e0b1ab5
Pablo <pablodelolmodecasas@gmail.com>
2024-01-25 19:34:20 +0100

    refactor

--- Projects/Libft/ft_strncmp.c
+++ Projects/Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 18:04:21 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/01/25 18:51:30 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/01/25 19:31:49 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i + 1 < n)
-	{
 		i++;
-	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
