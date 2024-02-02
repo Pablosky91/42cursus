9590fe071d1227f9646fcd009457c2dde96b55db
Pablo <pablodelolmodecasas@gmail.com>
2024-02-02 21:01:52 +0100

    created lstnew

---
+++ Projects/Libft/ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 20:57:06 by pdel-olm          #+#    #+#             */
+/*   Updated: 2024/02/02 20:59:58 by pdel-olm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Allocates (with malloc(3)) and returns a new node.
+The member variable ’content’ is initialized
+	with the value of the parameter ’content’.
+The variable ’next’ is initialized to NULL.
+*/
+t_list	*ft_lstnew(void *content)
+{
+	return (0);
+}
