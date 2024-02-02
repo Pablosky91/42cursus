4e6634f5778b07e4e8c06202d93202b449da0297
Pablo <pablodelolmodecasas@gmail.com>
2024-02-02 21:27:26 +0100

    created lstadd_front

---
+++ Projects/Libft/ft_lstadd_front.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 21:26:05 by pdel-olm          #+#    #+#             */
+/*   Updated: 2024/02/02 21:27:09 by pdel-olm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Adds the node ’new’ at the beginning of the list.
+*/
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	return (0);
+}
