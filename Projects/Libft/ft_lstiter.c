c30c9d4ca89c6b675b371b1102113f45ad707dab
Pablo <pablodelolmodecasas@gmail.com>
2024-02-02 21:37:49 +0100

    created lstiter

---
+++ Projects/Libft/ft_lstiter.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 21:36:19 by pdel-olm          #+#    #+#             */
+/*   Updated: 2024/02/02 21:37:35 by pdel-olm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Iterates the list ’lst’ and applies the function ’f’
+	on the content of each node.
+*/
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	return (0);
+}
