079ffc4d32bbcfff41803db2a580b6081e2d19bc
Pablo <pablodelolmodecasas@gmail.com>
2024-02-08 20:21:48 +0100

    lstmap

--- Projects/Libft/ft_lstmap_bonus.c
+++ Projects/Libft/ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 21:38:20 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/02/02 21:39:11 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/02/08 20:13:34 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,22 @@ The ’del’ function is used to delete the content of a node if needed.
 */
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	return (0);
+	t_list	*new_list;
+	t_list	*node;
+
+	if (!lst || !f || !del)
+		return (0);
+	new_list = 0;
+	while (lst)
+	{
+		node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&new_list, del);
+			return (0);
+		}
+		ft_lstadd_back(&new_list, node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
