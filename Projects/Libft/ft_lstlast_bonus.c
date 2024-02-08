0bdeea3404776c64bc91034880cb564a13ec3e40
Pablo <pablodelolmodecasas@gmail.com>
2024-02-08 17:46:50 +0100

    lstlast

--- Projects/Libft/ft_lstlast_bonus.c
+++ Projects/Libft/ft_lstlast_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 21:29:45 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/02/02 21:30:14 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/02/08 17:46:17 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,9 @@ Returns the last node of the list.
 */
 t_list	*ft_lstlast(t_list *lst)
 {
-	return (0);
+	if (!lst)
+		return (0);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
