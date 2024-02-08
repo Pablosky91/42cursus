ada820681e266ca161c85df28c576cf222d92f35
Pablo <pablodelolmodecasas@gmail.com>
2024-02-08 17:36:57 +0100

    lstaddfront

--- Projects/Libft/ft_lstadd_front_bonus.c
+++ Projects/Libft/ft_lstadd_front_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 21:26:05 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/02/02 21:27:09 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/02/08 17:17:24 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ Adds the node ’new’ at the beginning of the list.
 */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	return (0);
+	new->next = *lst;
+	*lst = new;
 }
