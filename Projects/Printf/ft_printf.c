b9821b6c409b91ee0c1d01f441d3b0a667f8fdf8
Pablo <pablodelolmodecasas@gmail.com>
2024-02-22 12:41:30 +0100

    main while loop

--- Projects/Printf/ft_printf.c
+++ Projects/Printf/ft_printf.c
@@ -17,18 +17,32 @@
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
+	size_t	i;
+	size_t	written_chars;
 
+	i = 0;
+	written_chars = 0;
 	va_start(args, format);
-	printf("%s --> %s\n", format, va_arg(args, char *));
+	while (format[i])
+	{
+		if (format[i] == "%")
+		{
+			i++;
+			written_chars += print_flag(format[i], args);
+		}
+		else
+			written_chars += print(format[i]);
+		i++;
+	}
 	va_end(args);
-	return (0);
+	return (written_chars);
 }
-
+/*
 int	main(void)
 {
 	char	*arg2;
 
 	arg2 = ft_strdup("Arg2");
-	ft_printf("Hola", "Arg1", arg2);
+	ft_printf("Hello", "Arg1", arg2);
 	return (0);
-}
+}*/
