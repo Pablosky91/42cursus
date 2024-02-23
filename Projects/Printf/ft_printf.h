807f07ef890e8a0a51f71bb31de81b45b88cdde9
Pablo <pablodelolmodecasas@gmail.com>
2024-02-24 00:03:29 +0100

    rename .h

--- Projects/Printf/ft_printf.h
+++ Projects/Printf/ft_printf.h
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 18:14:16 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/02/23 18:51:02 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/02/24 00:01:08 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include "libft/libft.h"
-#include <stdarg.h>
+# include <stdarg.h>
 
-int ft_print_string(char *str);
+# define STDOUT 1
+# define IS_UPPER 0
+# define IS_LOWER 32
+# define PRINT_NULL "(null)"
+# define PRINTF_SPECIFIER '%'
+
+int	ft_print_string(char *str);
 int	ft_print(char c);
+int	ft_print_char(char c);
 int	ft_print_flag(char flag, va_list args);
 int	ft_printf(const char *format, ...);
 
