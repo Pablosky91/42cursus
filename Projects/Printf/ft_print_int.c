07961016240daa267103858f006ea64f24b3d8c5
Pablo <pablodelolmodecasas@gmail.com>
2024-02-28 00:45:01 +0100

    int

--- Projects/Printf/ft_print_int.c
+++ Projects/Printf/ft_print_int.c
@@ -6,14 +6,36 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 00:09:30 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/02/24 00:21:27 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:16:49 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	ft_print_int_recursive(int nbr, int sign);
+
 int	ft_print_int(int nbr)
 {
-	(void) nbr;
-	return (-1000);
+	int	count;
+
+	count = 0;
+	if (nbr < 0)
+	{
+		count += ft_print('-');
+		count += ft_print_int_recursive(nbr, -1);
+	}
+	else
+		count += ft_print_int_recursive(nbr, 1);
+	return (count);
+}
+
+static int	ft_print_int_recursive(int nbr, int sign)
+{
+	int	count;
+
+	count = 0;
+	if (nbr >= 10 || nbr <= -10)
+		count += ft_print_int_recursive(nbr / 10, sign);
+	count += ft_print((nbr % 10) * sign + '0');
+	return (count);
 }
