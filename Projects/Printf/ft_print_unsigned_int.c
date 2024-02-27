59c9f398b91a6bd48db4a73805d31a4e9c32d119
Pablo <pablodelolmodecasas@gmail.com>
2024-02-28 00:44:49 +0100

    unsigned

--- Projects/Printf/ft_print_unsigned_int.c
+++ Projects/Printf/ft_print_unsigned_int.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 00:10:01 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/02/24 00:21:42 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/02/27 16:59:33 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int	ft_print_unsigned_int(unsigned int nbr)
 {
-	(void) nbr;
-	return (-1000);
+	int	count;
+
+	count = 0;
+	if (nbr >= 10)
+		count += ft_print_unsigned_int(nbr / 10);
+	count += ft_print((nbr % 10) + '0');
+	return (count);
 }
