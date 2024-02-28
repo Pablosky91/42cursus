36573f31da3e915a5aaebf1c1d627de8789f32e9
Pablo <pablodelolmodecasas@gmail.com>
2024-02-28 01:01:17 +0100

    else if flags

--- Projects/Printf/ft_print_flag.c
+++ Projects/Printf/ft_print_flag.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 18:50:23 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/02/27 14:09:58 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/02/28 01:00:08 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ int	ft_print_flag(char flag, va_list args)
 {
 	if (flag == 'c')
 		return (ft_print_char(va_arg(args, int)));
-	if (flag == 's')
+	else if (flag == 's')
 		return (ft_print_string(va_arg(args, char *)));
-	if (flag == 'p')
+	else if (flag == 'p')
 		return (ft_print_pointer(va_arg(args, size_t)));
-	if (flag == 'd' || flag == 'i')
+	else if (flag == 'd' || flag == 'i')
 		return (ft_print_int(va_arg(args, int)));
-	if (flag == 'u')
+	else if (flag == 'u')
 		return (ft_print_unsigned_int(va_arg(args, unsigned int)));
-	if (flag == 'x')
+	else if (flag == 'x')
 		return (ft_print_hexadecimal(va_arg(args, unsigned int), HEX_LOWER));
-	if (flag == 'X')
+	else if (flag == 'X')
 		return (ft_print_hexadecimal(va_arg(args, unsigned int), HEX_UPPER));
-	if (flag == '%')
+	else if (flag == '%')
 		return (ft_print_char(PRINTF_SPECIFIER));
 	return (0);
 }
