0dbf5b2e8ab21dc0310fbdcc716464c3d1e68693
Pablo <pablodelolmodecasas@gmail.com>
2024-03-24 19:37:03 +0100

    norminette

--- Projects/Get_Next_Line/get_next_line.c
+++ Projects/Get_Next_Line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 18:57:13 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/03/22 18:29:12 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/03/24 19:34:46 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,44 +31,39 @@ static char	*return_line(char **s)
 	return (line);
 }
 
-/*static */char	*loop(/*int fd, char *content, char *buffer*/)
+static char	*loop(int fd, char **content)
 {
-	int	bytes_read;
-	int	last_pos;
+	int		bytes_read;
+	int		last_pos;
+	char	*buffer;
 
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (free_and_null(content));
 	last_pos = BUFFER_SIZE;
-	bytes_read = -2;
-	
-	return (0);
+	bytes_read = 2;
+	while (last_pos == BUFFER_SIZE && bytes_read != 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (free_and_null(&buffer), free_and_null(content));
+		buffer[bytes_read] = 0;
+		*content = gnl_strjoin(*content, buffer, bytes_read);
+		if (!*content)
+			return (free_and_null(&buffer), free_and_null(content));
+		last_pos = gnl_strlen(buffer, 1);
+	}
+	return (free_and_null(&buffer), return_line(content));
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*content;
-	char		*buffer;
-	int			bytes_read;
-	int			last_pos;
 
 	init_empty_string(&content);
 	if (!content || fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	if (gnl_strlen(content, 0) != gnl_strlen(content, 1))
 		return (return_line(&content));
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (free_and_null(&content));
-	last_pos = BUFFER_SIZE;
-	bytes_read = -2;
-	while (last_pos == BUFFER_SIZE && bytes_read != 0)
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read < 0)
-			return (free_and_null(&buffer), free_and_null(&content));
-		buffer[bytes_read] = 0;
-		content = gnl_strjoin(content, buffer, bytes_read);
-		if (!content)
-			return (free_and_null(&buffer), free_and_null(&content));
-		last_pos = gnl_strlen(buffer, 1);
-	}
-	return (free_and_null(&buffer), return_line(&content));
+	return (loop(fd, &content));
 }
