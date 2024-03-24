bfb8a45feaf51c5a43d61ee823b5c30957056bd4
Pablo <pablodelolmodecasas@gmail.com>
2024-03-24 20:24:25 +0100

    gnl main for bonus

--- Projects/Test_GNL/main.c
+++ Projects/Test_GNL/main.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 12:26:38 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/03/21 16:17:51 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/03/24 20:23:47 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,43 @@ void leaks()
 	system("leaks test.out");
 }
 
+void read_lines (int fd, int num_lines)
+{
+	char *line = "";
+	int	i = 0;
+
+	while (i < num_lines)
+	{
+		line = get_next_line(fd);
+		printf("(fd %i): %s", fd, line);
+		free(line);
+		i++;
+	}
+	if (num_lines < 0)
+	{
+		while (line)
+		{
+			line = get_next_line(fd);
+			printf("(fd %i): %s", fd, line);
+			free(line);
+		}
+	}
+}
+
 int	main(void)
 {
 	atexit(leaks);
-	int fd = open("empty.txt", O_RDONLY);
-	char *line = "";
+	int fd1 = open("5shorts.txt", O_RDONLY);
+	int fd2 = open("3mediums.txt", O_RDONLY);
+	int fd3 = open("half.txt", O_RDONLY);
 
-	printf("BUFFER: %d\nfd: %d\n__________\n\n", BUFFER_SIZE, fd);
+	read_lines(fd1, 1);
+	read_lines(fd2, 2);
+	read_lines(fd3, -1);
+	read_lines(fd1, 2);
 
-	while (line)
-	{
-		line = get_next_line(fd);
-		printf("\tMAINline: %s", line);
-		free(line);
-	}
-	close(fd);
+	close(fd1);
+	close(fd2);
+	close(fd3);
 	return (0);
 }
