b97463ad4739065dc0e3819d55242746ee6bb91f
Pablo <pablodelolmodecasas@gmail.com>
2024-05-08 00:20:44 +0200

    list avoids repetition

--- Projects/Push_Swap/push_swap.c
+++ Projects/Push_Swap/push_swap.c
@@ -6,17 +6,19 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 12:10:05 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/05/07 22:01:39 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/05/08 00:19:41 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //TODO: avoid relink
+//TODO: protect movements
+//TODO: check leaks
 
 #include "push_swap.h"
 
 //static t_stacks	*create_mockup_stacks(t_stacks	*stacks);
 
-t_stack	*ft_ps_new(int content)
+t_stack	*ps_new(int content)
 {
 	t_stack	*node;
 
@@ -28,76 +30,64 @@ t_stack	*ft_ps_new(int content)
 	return (node);
 }
 
-static t_stack	*ft_ps_last(t_stack *stack)
-{
-	if (!stack)
-		return (0);
-	while (stack->next)
-		stack = stack->next;
-	return (stack);
-}
-
-void	ft_ps_add_back(t_stack **lst, t_stack *new)
+int	ps_add_back(t_stack **lst, int content)
 {
 	t_stack *aux;
-	
+	t_stack	*new_node;
+
+	new_node = ps_new(content);
+	if (!lst || !new_node)
+		return (0);
 	aux = *lst;
-	if (!lst || !new)
-		return ;
 	while (aux->next)
+	{
+		if (aux->content == content)
+			return (0);
 		aux = aux->next;
-	aux->next = new;
+	}
+	aux->next = new_node;
+	return (1);
 }
 
 
 
 
 
-void	create_mockup_stack(t_stack	**stack, int num1, int num2, int num3, int num4)
+int	create_mockup_stack(t_stack	**stack, int num1, int num2, int num3, int num4)
 {
-	t_stack	*aux;
-	
-	aux = *stack;
-	//int num = 1, num1 = 2, num2 = 3;
-	aux = ft_ps_new(num1);
-	//printf("sd");
-	//printf("%d", *(int *)aux->content);
-	ft_ps_add_back(&aux, ft_ps_new(num2));
-	//printf("%d", *(int *)aux->next->content);
-	ft_ps_add_back(&aux, ft_ps_new(num3));
-	ft_ps_add_back(&aux, ft_ps_new(num4));
-	/*ft_lstadd_back(&(stacks)->stack_a, ft_lstnew((void *)3));
-	(stacks)->stack_b = ft_lstnew((void *)5);
-	ft_lstadd_back(&(stacks)->stack_b, ft_lstnew((void *)8));
-	ft_lstadd_back(&(stacks)->stack_b, ft_lstnew((void *)7));
-	ft_lstadd_back(&(stacks)->stack_b, ft_lstnew((void *)6)); */
-	*stack = aux;
-	//show_stacks(*stacks);
-//	return (&stacks);
+	*stack = ps_new(num1);
+	if (!*stack)
+		return (0);
+	if (!ps_add_back(stack, num2))
+		return (0);
+	if (!ps_add_back(stack, num3))
+		return (0);
+	if (!ps_add_back(stack, num4))
+		return (0);
+	return (1);
 }
 int	main(void)
 {
-	//t_stacks	*stacks;
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
-	create_mockup_stack(&stack_a, 1, 2, 3, 4);
-	create_mockup_stack(&stack_b, 4, 5, 6, 7);
+	if (!create_mockup_stack(&stack_a, 1, 2, 3, 4))
+		return (ft_printf("Error\n"), 42);
+	if (!create_mockup_stack(&stack_b, 4, 5, 6, 7))
+		return (ft_printf("Error\n"), 42);
+	show_stacks(stack_a, stack_b);
 	
-	show_stacks(stack_a, stack_b);
 	moves(&stack_a, &stack_b, pb);
-	/* if (!stacks.stack_a)
-		ft_printf("ERROR");
-	ft_printf("main: %i\n", stacks.stack_a->content); */
 	show_stacks(stack_a, stack_b);
-	/* char *sentence = "  f    g  ";
-	char delimiter = ' ';	
-	char **array = ft_split(sentence, delimiter);
-	int i = 0;
-	while (array[i])
-	{
-		ft_printf("%i: >%s<\n", i, array[i]);
-		i++;
-	} */
+
+	moves(&stack_a, &stack_b, pb);
+	show_stacks(stack_a, stack_b);
+
+	moves(&stack_a, &stack_b, pb);
+	show_stacks(stack_a, stack_b);
+
+	moves(&stack_a, &stack_b, pb);
+	show_stacks(stack_a, stack_b);
+
 	return (0);
 }
