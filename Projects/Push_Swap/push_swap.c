f02b4fad49b7b7715735f3a0c137292213681a59
Pablo <pablodelolmodecasas@gmail.com>
2024-06-11 20:56:14 +0200

    trying out Three-Way Quick Sort

--- Projects/Push_Swap/push_swap.c
+++ Projects/Push_Swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 12:10:05 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/05/30 20:19:47 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/06/11 18:45:43 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static void		error(t_stack **stack);
 //TODO delete
 void	show_stacks(t_stack *stack_a, t_stack *stack_b)
 {
-	ft_printf("a b\n---\n");
+	ft_printf("a b\n----\n");
 	while (stack_a || stack_b)
 	{
 		if (stack_a)
@@ -50,7 +50,43 @@ void	show_stacks(t_stack *stack_a, t_stack *stack_b)
 		else
 			ft_printf("\n");
 	}
-	ft_printf("---\n\n");
+	ft_printf("----\n\n");
+}
+
+//TODO delete
+void move(t_stack **stack_a, t_stack **stack_b, int kase)
+{
+	switch (kase)
+	{
+	case 1:
+		moves(stack_a, stack_b, ra);
+		break;
+	case 2:
+		moves(stack_a, stack_b, pb);
+		break;
+	case 3:
+		moves(stack_a, stack_b, pb);
+		moves(stack_a, stack_b, rb);
+		break;
+	case 4:
+		moves(stack_a, stack_b, pa);
+		break;
+	case 5:
+		moves(stack_a, stack_b, pa);
+		moves(stack_a, stack_b, ra);
+		break;
+	case 6:
+		moves(stack_a, stack_b, rb);
+		break;
+	case 7:
+		moves(stack_a, stack_b, rrb);
+		break;
+	case 8:
+		moves(stack_a, stack_b, rra);
+		break;
+	}
+	//printf("\n");
+	//show_stacks(*stack_a, *stack_b);
 }
 
 int	main(int argc, char **argv)
@@ -66,7 +102,7 @@ int	main(int argc, char **argv)
 
 
 
-	//show_stacks(stack_a, stack_b);
+	show_stacks(stack_a, stack_b);
 	sort(&stack_a, &stack_b);
 	//show_stacks(stack_a, stack_b);
 	/* moves(&stack_a, &stack_b, sa);
@@ -77,12 +113,100 @@ int	main(int argc, char **argv)
 	show_stacks(stack_a, stack_b);
 	moves(&stack_a, &stack_b, sa); */
 
+//high 4, mid 5, low 6
+//high 1, mid 2, low 3
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 3);
+	show_stacks(stack_a, stack_b);
+//group 1
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 3);
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 1);
+	show_stacks(stack_a, stack_b);
+//group 1-1 (25 26 27)
+	move(&stack_a, &stack_b, 1);
+	move(&stack_a, &stack_b, 2);
+	move(&stack_a, &stack_b, 3);
+	show_stacks(stack_a, stack_b);
+//retrieve group 1-1
+	move(&stack_a, &stack_b, 8);//nada
+	move(&stack_a, &stack_b, 4);
+	move(&stack_a, &stack_b, 7);
+	move(&stack_a, &stack_b, 4);
+	show_stacks(stack_a, stack_b);
+//group 1-2 (22 23 24)
+	move(&stack_a, &stack_b, 6);
+	move(&stack_a, &stack_b, 4);
+	move(&stack_a, &stack_b, 5);
+	show_stacks(stack_a, stack_b);
+//retrieve group 1-2
+	move(&stack_a, &stack_b, 8);
+	move(&stack_a, &stack_b, 7);
+	move(&stack_a, &stack_b, 4);
+	show_stacks(stack_a, stack_b);
+//group 1-3 (19 20 21)
+	move(&stack_a, &stack_b, 7);
+	move(&stack_a, &stack_b, 7);
+	move(&stack_a, &stack_b, 4);
+	move(&stack_a, &stack_b, 7);
+	move(&stack_a, &stack_b, 5);
+	show_stacks(stack_a, stack_b);
+//retrieve group 1-3
+	move(&stack_a, &stack_b, 8);
+	move(&stack_a, &stack_b, 4);
+	show_stacks(stack_a, stack_b);
+
+//group 2
+	move(&stack_a, &stack_b, 5);
+	move(&stack_a, &stack_b, 6);
+	move(&stack_a, &stack_b, 4);
+	move(&stack_a, &stack_b, 6);
+	move(&stack_a, &stack_b, 6);
+	move(&stack_a, &stack_b, 5);
+	move(&stack_a, &stack_b, 4);
+	move(&stack_a, &stack_b, 5);
+	move(&stack_a, &stack_b, 4);
+	show_stacks(stack_a, stack_b);
+
+
 
 
 
 	free_stack(&stack_a);
 	return (0);
 }
+
 
 static void	free_stack(t_stack **stack)
 {
