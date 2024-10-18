51e0ed199c11f0b0e047e85c87ebd2961f3d970c
Pablo <pablodelolmodecasas@gmail.com>
2024-10-18 16:04:39 +0200

    retry and change speed

--- Projects/So_Long/src_bonus/window_bonus/my_key_hook_bonus.c
+++ Projects/So_Long/src_bonus/window_bonus/my_key_hook_bonus.c
@@ -3,24 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   my_key_hook_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pdel-olm <pdel-olm@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pdel-olm <pdel-olm@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 19:29:20 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/10/14 21:19:40 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2024/10/18 16:02:32 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
+
+static void	change_speed(mlx_key_data_t keydata, t_game *game);
 
 void	my_key_hook(mlx_key_data_t keydata, void *param)
 {
 	t_game	*game;
 
 	game = param;
+	if (keydata.action != MLX_PRESS)
+		return ;
 	if (keydata.key == MLX_KEY_ESCAPE)
 		exit_game(game, SL_OK);
-	if (keydata.action != MLX_PRESS || game->penguin->facing != STILL)
-		return ;
+	if (keydata.key == MLX_KEY_R)
+		retry(game);
+	change_speed(keydata, game);
 	if (keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
 		start_movement(game, NORTH);
 	else if (keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT)
@@ -29,4 +34,15 @@ void	my_key_hook(mlx_key_data_t keydata, void *param)
 		start_movement(game, SOUTH);
 	else if (keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT)
 		start_movement(game, EAST);
+}
+
+//TODO doc
+static void	change_speed(mlx_key_data_t keydata, t_game *game)
+{
+	if (keydata.key == MLX_KEY_0 || keydata.key == MLX_KEY_KP_0)
+		game->speed = game->img_size;
+	else if (keydata.key >= MLX_KEY_1 && keydata.key <= MLX_KEY_9)
+		game->speed = game->img_size * (keydata.key - MLX_KEY_0) / 10;
+	else if (keydata.key >= MLX_KEY_KP_1 && keydata.key <= MLX_KEY_KP_9)
+		game->speed = game->img_size * (keydata.key - MLX_KEY_KP_0) / 10;
 }
---
+++ Projects/So_Long/src_bonus/window_bonus/retry_bonus.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   retry_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdel-olm <pdel-olm@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 15:18:55 by pdel-olm          #+#    #+#             */
+/*   Updated: 2024/10/18 15:12:16 by pdel-olm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+void	retry(t_game *game)
+{
+	int	id_fish;
+
+	if (game->moves == 0)
+		return ;
+	game->penguin->y = game->initial_row * game->img_size;
+	game->penguin->x = game->initial_col * game->img_size;
+	game->penguin->facing = STILL;
+	id_fish = 0;
+	while (id_fish < game->quantity_fishes)
+	{
+		game->fishes[id_fish]->alive->enabled = true;
+		game->fishes[id_fish]->dead->enabled = false;
+		game->fishes[id_fish]->collected = false;
+		id_fish++;
+	}
+	game->collected_fishes = 0;
+	game->home->closed->enabled = true;
+	game->home->open->enabled = false;
+	game->moves = 0;
+	ft_printf("---------\n");
+}
