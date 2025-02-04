b6512ac9c6c7519bf1ec576c317f7c3133440723
Pablo <pablodelolmodecasas@gmail.com>
2025-02-04 17:15:39 +0100

    first here_doc prototype

--- Projects/Pipex/src/pipex.c
+++ Projects/Pipex/src/pipex.c
@@ -1,16 +1,30 @@
 /* ************************************************************************** */
-/*																			*/
-/*														:::	  ::::::::   */
-/*   pipex.c											:+:	  :+:	:+:   */
-/*													+:+ +:+		 +:+	 */
-/*   By: pdel-olm <pdel-olm@student.42madrid.com>   +#+  +:+	   +#+		*/
-/*												+#+#+#+#+#+   +#+		   */
-/*   Created: 2025/01/16 12:19:39 by pdel-olm		  #+#	#+#			 */
-/*   Updated: 2025/01/16 12:46:58 by pdel-olm		 ###   ########.fr	   */
-/*																			*/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdel-olm <pdel-olm@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 12:19:39 by pdel-olm          #+#    #+#             */
+/*   Updated: 2025/02/04 17:09:11 by pdel-olm         ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+bool	is_delimiter_line(char *delimiter, char *line)
+{
+	int	iter;
+
+	iter = 0;
+	while (delimiter[iter] && line[iter])
+	{
+		if (delimiter[iter] != line[iter])
+			return (false);
+		iter++;
+	}
+	return (!delimiter[iter] && line[iter] == '\n' && !line[iter + 1]);
+}
 
 //TODO investigate what happens if path is NULL
 char	**get_path(char **envp)
@@ -75,14 +89,14 @@ char	*get_path_command(char **path, char *command)
 	return (NULL);
 }
 
-bool	is_first_command(int n_cmd)
+bool	reads_from_infile(t_pipex pipex, int cmd_pos)
 {
-	return (n_cmd == 2);
+	return (cmd_pos == pipex.first_command_pos && pipex.infile);
 }
 
-bool	is_last_command(t_pipex pipex, int n_cmd)
+bool	writes_to_outfile(t_pipex pipex, int cmd_pos)
 {
-	return (n_cmd == pipex.argc - 2);
+	return (cmd_pos == pipex.argc - 2);
 }
 
 void	dup2_close(int fd1, int fd2)
@@ -91,78 +105,116 @@ void	dup2_close(int fd1, int fd2)
 	close(fd1);
 }
 
-void	child(t_pipex pipex, int n_cmd, int *new_fds, int *old_fds)
+void	child(t_pipex pipex, int cmd_pos, int *write_pipe, int *read_pipe)
 {
 	char	**cmd;
 	int		infile_fd;
 	int		outfile_fd;
 
-	if (is_first_command(n_cmd))
+	if (reads_from_infile(pipex, cmd_pos))
 	{
-		infile_fd = open(pipex.argv[1], O_RDONLY);
+		infile_fd = open(pipex.infile, O_RDONLY);
 		dup2_close(infile_fd, STDIN_FILENO);
 	}
 	else
 	{
-		dup2_close(old_fds[PIPE_READ], STDIN_FILENO);
-		close(old_fds[PIPE_WRITE]);
+		dup2_close(read_pipe[PIPE_READ], STDIN_FILENO);
+		close(read_pipe[PIPE_WRITE]);
 	}
-	if (is_last_command(pipex, n_cmd))
+	if (writes_to_outfile(pipex, cmd_pos))
 	{
-		outfile_fd = open(pipex.argv[pipex.argc - 1], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+		outfile_fd = open(pipex.outfile, pipex.outfile_flag, 0644);
 		dup2_close(outfile_fd, STDOUT_FILENO);
 	}
 	else
 	{
-		dup2_close(new_fds[PIPE_WRITE], STDOUT_FILENO);
-		close(new_fds[PIPE_READ]);
+		dup2_close(write_pipe[PIPE_WRITE], STDOUT_FILENO);
+		close(write_pipe[PIPE_READ]);
 	}
-	cmd = ft_split(pipex.argv[n_cmd], ' ');
+	cmd = ft_split(pipex.argv[cmd_pos], ' ');
 	execve(get_path_command(pipex.path, cmd[0]), cmd, pipex.envp);
 }
 
-int	*execute_command(t_pipex pipex, int n_cmd, int *old_fds)
+int	*execute_command(t_pipex pipex, int cmd_pos, int *read_pipe)
 {
-	int		*new_fds;
+	int		*write_pipe;
 	pid_t	pid;
 
-	new_fds = NULL;
-	if (!is_last_command(pipex, n_cmd))
+	write_pipe = NULL;
+	if (!writes_to_outfile(pipex, cmd_pos))
 	{
-		new_fds = malloc(2 * sizeof(int));
-		pipe(new_fds);
+		write_pipe = malloc(2 * sizeof(int));
+		pipe(write_pipe);
 	}
 	pid = fork();
 	if (pid == 0)
-		child(pipex, n_cmd, new_fds, old_fds);
-	if (!is_first_command(n_cmd))
+		child(pipex, cmd_pos, write_pipe, read_pipe);
+	if (!reads_from_infile(pipex, cmd_pos))
 	{
-		close(old_fds[PIPE_WRITE]);
-		close(old_fds[PIPE_READ]);
+		close(read_pipe[PIPE_WRITE]);
+		close(read_pipe[PIPE_READ]);
+		free(read_pipe);
 	}
-	free(old_fds);
-	return (new_fds);
+	return (write_pipe);
+}
+
+int	*here_doc(t_pipex *pipex)
+{
+	int		*fds;
+	char	*line;
+	bool	end;
+
+	pipex->infile = NULL;
+	pipex->outfile_flag = O_CREAT | O_APPEND | O_WRONLY;
+	pipex->first_command_pos = 3;
+	fds = malloc(2 * sizeof(int));
+	end = false;
+	pipe(fds);
+	while (!end)
+	{
+		ft_printf("%s", PIPE_HEREDOC);
+		line = get_next_line(STDIN_FILENO);
+		end = !line || is_delimiter_line(pipex->argv[2], line);
+		if (!end)
+			write(fds[PIPE_WRITE], line, ft_strlen(line));
+		free(line);
+	}
+	return (fds);
+}
+
+int	*init(t_pipex *pipex, int argc, char **argv, char **envp)
+{
+	int		*fds;
+
+	fds = NULL;
+	pipex->argc = argc;
+	pipex->argv = argv;
+	pipex->envp = envp;
+	pipex->path = get_path(envp);
+	pipex->infile = argv[1];
+	pipex->outfile = argv[argc - 1];
+	pipex->outfile_flag = O_CREAT | O_TRUNC | O_WRONLY;
+	pipex->first_command_pos = 2;
+	if (ft_str_equals(argv[1], HERE_DOC))
+		fds = here_doc(pipex);
+	return (fds);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	pipex;
-	int		n_cmd;
 	int		*fds;
+	int		cmd_pos;
 
 	if (argc < 5)
 	{
 		ft_putendl_fd("Usage: ./pipex file1 cmd1 cmd2 file2", 2);
 		exit(1);
 	}
-	pipex.argv = argv;
-	pipex.argc = argc;
-	pipex.envp = envp;
-	pipex.path = get_path(envp);
-	n_cmd = 1;
-	fds = NULL;
-	while (++n_cmd < argc - 1)
-		fds = execute_command(pipex, n_cmd, fds);
+	fds = init(&pipex, argc, argv, envp);
+	cmd_pos = pipex.first_command_pos;
+	while (cmd_pos < argc - 1)
+		fds = execute_command(pipex, cmd_pos++, fds);
 	while (wait(NULL) != -1)
 		;
 	ft_free_double_pointer((void **) pipex.path);
--- Projects/Pipex/src/pipex.h
+++ Projects/Pipex/src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 16:57:50 by pdel-olm          #+#    #+#             */
-/*   Updated: 2025/01/24 19:54:06 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2025/02/04 17:00:21 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,19 @@
 # define PIPE_READ	STDIN_FILENO
 # define PIPE_WRITE	STDOUT_FILENO
 
+# define PIPE_HEREDOC "\e[1mpipe heredoc> \e[0m"
+# define HERE_DOC "here_doc"
+
 typedef struct s_pipex
 {
 	int		argc;
 	char	**argv;
 	char	**envp;
 	char	**path;
+	char	*infile;
+	char	*outfile;
+	int		outfile_flag;
+	int		first_command_pos;
 }	t_pipex;
 
 #endif
