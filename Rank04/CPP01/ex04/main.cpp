af8e4143223f7cb17bac35ffa28e33e18b927a31
Pablo <pablodelolmodecasas@gmail.com>
2026-01-04 01:00:11 +0100

    cpp01ex04

--- Rank04/CPP01/ex04/main.cpp
+++ Rank04/CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pdel-olm <pdel-olm@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 20:53:15 by pdel-olm          #+#    #+#             */
-/*   Updated: 2025/09/29 19:54:47 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2026/01/04 00:53:06 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,38 @@ int	main(int argc, char **argv)
 	std::string infile = argv[1];
 	std::string outfile = infile + ".replace";
 	std::string s1 = argv[2];
+	if (s1.empty())
+	{
+		std::cerr << "The string to replace may not be empty" << std::endl;
+		return (2);
+	}
 	std::string s2 = argv[3];
-	std::ifstream file_in(infile.c_str()); // Open a file to read
+	std::ifstream file_in(infile.c_str());
 	if (!file_in)
 	{
 		std::perror((((std::string)"Could not open file ").append(infile)).c_str());
-		return (2);
+		return (3);
 	}
-	std::ofstream file_out(outfile.c_str()); // Open a file to write
+	std::ofstream file_out(outfile.c_str());
 	if (!file_out)
 	{
 		std::perror((((std::string)"Could not open file ").append(outfile)).c_str());
 		file_in.close();
-		return (3);
+		return (4);
 	}
 	std::string line;
 	while (std::getline(file_in, line))
 	{
-		file_out << line << std::endl;
+		std::size_t pos = line.find(s1);
+		while (pos != line.npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos = line.find(s1, pos + 1);
+		}
+		file_out << line;
+		if (!file_in.eof())
+			file_out << std::endl;
 	}
 	file_in.close();
 	file_out.close();
